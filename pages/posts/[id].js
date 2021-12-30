6684e46cc6fbda5d9efdff07cb379db6c6e224dc
Fabio Miranda <fabio@miranti.net.br>
2021-12-30 18:46:44 -0300

    Render markdown content, format date, polish the post markup

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,7 @@
+import Head from 'next/head'
+import Date from '../../components/date'
 import Layout from '../../components/layout'
+import utilStyles from '../../styles/utils.module.sass'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticPaths() {
@@ -6,17 +9,22 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  return { props: { postData: getPostData(params.id) } }
+  return { props: { postData: await getPostData(params.id) } }
 }
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
